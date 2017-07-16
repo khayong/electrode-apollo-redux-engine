f06291cab6783e5134bfa68c6f3c98173b491ad6
Tan Khay Ong <khayong@gmail.com>
2017-07-16 17:34:28 +0800

    Server rendering for Apollo.

--- test/spec/combine.spec.js
+++ test/spec/combine.spec.js
@@ -2,61 +2,69 @@
 
 const createStore = require("redux").createStore;
 const combineReducers = require("redux").combineReducers;
+const applyMiddleware = require("redux").applyMiddleware;
+const compose = require("redux").compose;
 const ApolloClient = require("react-apollo").ApolloClient;
 
 const {combineReducerWithApollo} = require("../..");
 
 const checkBox = (store, action) => {
-    if (action.type === "TOGGLE_CHECK") {
-        return {
-            checked: !store.checked
-        };
-    }
+  if (action.type === "TOGGLE_CHECK") {
+    return {
+      checked: !store.checked
+    };
+  }
 
-    return store || {checked: false};
+  return store || {checked: false};
 };
 
 const number = (store, action) => {
-    if (action.type === "INC_NUMBER") {
-        return {
-            value: store.value + 1
-        };
-    } else if (action.type === "DEC_NUMBER") {
-        return {
-            value: store.value - 1
-        };
-    }
+  if (action.type === "INC_NUMBER") {
+    return {
+      value: store.value + 1
+    };
+  } else if (action.type === "DEC_NUMBER") {
+    return {
+      value: store.value - 1
+    };
+  }
 
-    return store || {value: 0};
+  return store || {value: 0};
 };
 
 describe("Combine reducer with Apollo", function () {
 
-    it("should combine existing root reducer", () => {
-        const client = new ApolloClient();
+  it("should combine existing root reducer", () => {
+    const client = new ApolloClient();
 
-        const rootReducer = combineReducers({
-            checkBox,
-            number
-        });
-
-        const initialState = {
-            checkBox: {checked: false},
-            number: {value: 999}
-        };
-        const store = createStore(combineReducerWithApollo(rootReducer, client), initialState);
-        expect(store.getState()).to.have.all.keys("checkBox", "number", "apollo");
-        expect(store.getState().checkBox).to.deep.equal({checked: false});
-        expect(store.getState().number).to.deep.equal({value: 999});
-
-        store.dispatch({type: "TOGGLE_CHECK"});
-        expect(store.getState().checkBox).to.deep.equal({checked: true});
-        store.dispatch({type: "TOGGLE_CHECK"});
-        expect(store.getState().checkBox).to.deep.equal({checked: false});
-
-        store.dispatch({type: "INC_NUMBER"});
-        expect(store.getState().number).to.deep.equal({value: 1000});
-        store.dispatch({type: "DEC_NUMBER"});
-        expect(store.getState().number).to.deep.equal({value: 999});
+    const rootReducer = combineReducers({
+      checkBox,
+      number
     });
+
+    const initialState = {
+      checkBox: {checked: false},
+      number: {value: 999}
+    };
+    const store = createStore(
+      combineReducerWithApollo(rootReducer, client),
+      initialState,
+      compose(
+        applyMiddleware(client.middleware())
+      )
+    );
+    expect(store.getState()).to.have.all.keys("checkBox", "number", "apollo");
+    expect(store.getState().checkBox).to.deep.equal({checked: false});
+    expect(store.getState().number).to.deep.equal({value: 999});
+
+    store.dispatch({type: "TOGGLE_CHECK"});
+    expect(store.getState().checkBox).to.deep.equal({checked: true});
+    store.dispatch({type: "TOGGLE_CHECK"});
+    expect(store.getState().checkBox).to.deep.equal({checked: false});
+
+    store.dispatch({type: "INC_NUMBER"});
+    expect(store.getState().number).to.deep.equal({value: 1000});
+    store.dispatch({type: "DEC_NUMBER"});
+    expect(store.getState().number).to.deep.equal({value: 999});
+  });
 });
--- test/spec/render.spec.js
+++ test/spec/render.spec.js
@@ -2,6 +2,8 @@
 
 const createStore = require("redux").createStore;
 const combineReducers = require("redux").combineReducers;
+const applyMiddleware = require("redux").applyMiddleware;
+const compose = require("redux").compose;
 const ApolloClient = require("react-apollo").ApolloClient;
 const ReduxRouterEngine = require("electrode-redux-router-engine");
 
@@ -13,95 +15,122 @@ require("babel-register");
 const routes = require("../routes.jsx").default;
 
 const checkBox = (store, action) => {
-    if (action.type === "TOGGLE_CHECK") {
-        return {
-            checked: !store.checked
-        };
-    }
+  if (action.type === "TOGGLE_CHECK") {
+    return {
+      checked: !store.checked
+    };
+  }
 
-    return store || {checked: false};
+  return store || {checked: false};
 };
 
 const number = (store, action) => {
-    if (action.type === "INC_NUMBER") {
-        return {
-            value: store.value + 1
-        };
-    } else if (action.type === "DEC_NUMBER") {
-        return {
-            value: store.value - 1
-        };
-    }
+  if (action.type === "INC_NUMBER") {
+    return {
+      value: store.value + 1
+    };
+  } else if (action.type === "DEC_NUMBER") {
+    return {
+      value: store.value - 1
+    };
+  }
 
-    return store || {value: 0};
+  return store || {value: 0};
 };
 
 const getPrefetchState = (prefetch) => {
-    const regex = /^window.__PRELOADED_STATE__ = (.*);$/g;
-    return JSON.parse(regex.exec(prefetch)[1]);
+  const regex = /^window.__PRELOADED_STATE__ = (.*);$/g;
+  return JSON.parse(regex.exec(prefetch)[1]);
 };
 
 describe("Server rendering with Apollo", function () {
 
-    it("should render to string without apollo", () => {
-        const rootReducer = combineReducers({
-            checkBox,
-            number
-        });
-        const createReduxStore = (req, match) => { // eslint-disable-line
-            const initialState = {
-                checkBox: {checked: false},
-                number: {value: 999}
-            };
-            const store = createStore(rootReducer, initialState);
-            return Promise.resolve(store);
-        };
-        const testReq = {
-            method: "get",
-            log: () => {
-            },
-            app: {},
-            url: {
-                path: "/test"
-            }
-        };
-
-        const engine = new ReduxRouterEngine({routes, createReduxStore, renderToString});
-        return engine.render(testReq).then(result => {
-            expect(getPrefetchState(result.prefetch)).to.deep.equal({checkBox: {checked: false}, number: {value: 999}});
-        });
+  it("should render to string without apollo", () => {
+    const rootReducer = combineReducers({
+      checkBox,
+      number
     });
+    const createReduxStore = (req, match) => { // eslint-disable-line
+      const initialState = {
+        checkBox: {checked: false},
+        number: {value: 999}
+      };
+      const store = createStore(rootReducer, initialState);
+      return Promise.resolve(store);
+    };
+    const testReq = {
+      method: "get",
+      log: () => {
+      },
+      app: {},
+      url: {
+        path: "/test"
+      }
+    };
 
-    it("should render to string with apollo", () => {
-        const client = new ApolloClient();
-
-        const rootReducer = combineReducers({
-            checkBox,
-            number
-        });
-        const createReduxStore = (req, match) => { // eslint-disable-line
-            const initialState = {
-                checkBox: {checked: false},
-                number: {value: 999}
-            };
-            const store = createStore(combineReducerWithApollo(rootReducer, client), initialState);
-            return Promise.resolve(store);
-        };
-        const testReq = {
-            method: "get",
-            log: () => {
-            },
-            app: {
-                apollo: client
-            },
-            url: {
-                path: "/test"
-            }
-        };
-
-        const engine = new ReduxRouterEngine({routes, createReduxStore, renderToString});
-        return engine.render(testReq).then(result => {
-            expect(getPrefetchState(result.prefetch)).to.have.all.keys(["checkBox", "number", "apollo"]);
-        });
+    const engine = new ReduxRouterEngine(
+      {routes, createReduxStore, renderToString});
+    return engine.render(testReq).then(result => {
+      expect(getPrefetchState(result.prefetch)).
+        to.
+        deep.
+        equal({checkBox: {checked: false}, number: {value: 999}});
     });
+  });
+
+  it("should render to string with apollo", () => {
+    const client = new ApolloClient();
+
+    const rootReducer = combineReducers({
+      checkBox,
+      number
+    });
+    const createReduxStore = (req, match) => { // eslint-disable-line
+      const initialState = {
+        checkBox: {checked: false},
+        number: {value: 999}
+      };
+      const store = createStore(
+        combineReducerWithApollo(rootReducer, client),
+        initialState,
+        compose(
+          applyMiddleware(client.middleware())
+        )
+      );
+      return Promise.resolve(store);
+    };
+    const testReq = {
+      method: "get",
+      log: () => {
+      },
+      app: {
+        apollo: client
+      },
+      url: {
+        path: "/test"
+      }
+    };
+
+    const engine = new ReduxRouterEngine(
+      {routes, createReduxStore, renderToString});
+    return engine.render(testReq).then(result => {
+      expect(getPrefetchState(result.prefetch)).
+        to.
+        have.
+        all.
+        keys(["checkBox", "number", "apollo"]);
+    });
+  });
+
+  /*it("should", () => {
+   const rootReducer = combineReducers({
+   checkBox,
+   number
+   });
+   const initialState = {
+   checkBox: {checked: false},
+   number: {value: 999}
+   };
+   const store = createStore(rootReducer, initialState);
+   });*/
 });
